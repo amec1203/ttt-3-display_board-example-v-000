3b1e7d19ce59ffeca3a43baec62647b7c0a8148e
Angela Campion <campion.angela@gmail.com>
2018-04-06 20:07:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+puts "Tic Tac Toe Board"
+def display_board
+  cell = "   "
+  pipe = "|"
+  separating_line = "-----------"
+  end 
+
+puts display_board
